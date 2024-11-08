93a50326784a50d755d04b47cb1b35be1e29eb8f
Artyom Uschenko <net775@yandex.ru>
2024-11-08 11:21:54 +0300

    Исправление кода от SQL инъекций

--- keyboard.py
+++ keyboard.py
@@ -1,12 +1,24 @@
 from aiogram import types
 
 def keyboard_start(start_keyboard=None):
-    start_keyboard = types.InlineKeyboardMarkup(row_width=2) # row_width=2 - Дизайн клавиатуры
+    start_keyboard = types.InlineKeyboardMarkup(row_width=1) # row_width=2 - Дизайн клавиатуры
     start_button1 = types.InlineKeyboardButton(text ='О нас', callback_data='start_about')
     start_button2 = types.InlineKeyboardButton(text ='Помощь от ИИ', callback_data='start_II')
     start_button3 = types.InlineKeyboardButton(text='Задачи на сегодня', callback_data='start_today')
     start_button4 = types.InlineKeyboardButton(text='Сайт', url='https://platform-eadsc.voskhod.ru/')
-    start_keyboard.add(start_button1, start_button2, start_button3, start_button4)
+    start_button5 = types.InlineKeyboardButton(text='Отметиться', callback_data='start_inwork')
+    start_keyboard.add(start_button1, start_button2, start_button3, start_button4, start_button5)
     return start_keyboard
 
+def keyboard_inwork():
+    inwork_keyboard = types.InlineKeyboardMarkup(row_width=2)
+    inwork_button1 = types.InlineKeyboardButton(text='Пришел', callback_data='inwork_in')
+    inwork_button2 = types.InlineKeyboardButton(text='Ушел', callback_data='inwork_out')
+    inwork_keyboard.add(inwork_button1, inwork_button2)
+    return inwork_keyboard
+
+
+
+
 keyboard_start = keyboard_start()
+keyboard_inwork = keyboard_inwork()
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import logging
+import datetime
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
 
 import sqlite3 as sq
 
-from keyboard import keyboard_start
+from keyboard import keyboard_start, keyboard_inwork
 
 # Объект бота
 bot = Bot(token='7301540230:AAEWyxaVCNqSZ0mqg5oxpbeMEiFGVBQJUdA')
@@ -21,13 +23,29 @@ async def on_startup(_):
         print('Connected to database')
     base.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER, Datatime REAL, Callback INTEGER, Link TEXT, PRIMARY KEY("ID" AUTOINCREMENT))')
     base.commit()
+    base.execute('CREATE TABLE IF NOT EXISTS inwork (id INTEGER, Datatime REAL, Callback INTEGER, Link TEXT, Hour INTEGER, Minute INTEGER, PRIMARY KEY("ID" AUTOINCREMENT))')
+    base.commit()
+    base.execute(
+        'CREATE TABLE IF NOT EXISTS outwork (id INTEGER, Datatime REAL, Callback INTEGER, Link TEXT, Hour INTEGER, Minute INTEGER, PRIMARY KEY("ID" AUTOINCREMENT))')
+    base.commit()
+    date = datetime.datetime.now()
+    hour = date.hour
+    minute = date.minute
+    print(hour, minute)
 
 # Хэндлер на команду /start
 async def start(message: types.Message):
-    await bot.send_message(message.from_user.id, f"Добрый день, {message.from_user.first_name}!\n\n Я - твой личный помощник по ГИС \"ЦХЭД\" ", reply_markup=keyboard_start )
-    # print(message)
-    # await bot.send_message(message.from_user.id, f"Привет {message.from_user.username}")
-    # # pass
+    user = cur.execute(f'SELECT Callback FROM users WHERE Callback == ?', (message.chat.id,)).fetchone()
+    if user == None:
+        dates = datetime.datetime.today()
+        sql_insert ="""INSERT INTO users (Datatime,Callback, Link) VALUES (?,?,?)"""
+        data_insert = (dates, message.chat.id, message.from_user.username)
+        cur.execute(sql_insert, data_insert)
+        base.commit()
+
+    await bot.send_message(message.from_user.id,
+                               f"Добрый день, {message.from_user.first_name}!\n\n Я - твой личный помощник по ГИС \"ЦХЭД\" ",
+                               reply_markup=keyboard_start)
 
 async def start_keyboard(callback: types.CallbackQuery):
     await bot.answer_callback_query(callback.id)
@@ -37,13 +55,41 @@ async def start_keyboard(callback: types.CallbackQuery):
     if str == 'II':
         pass
     if str == 'today':
-        link = cur.execute(f"SELECT Link FROM users WHERE Callback = '{callback.from_user.id}'").fetchone()
+        link = cur.execute(f"SELECT Link FROM users WHERE Callback = ?", (callback.from_user.id,)).fetchone()
         base.commit()
         await bot.send_message(callback.from_user.id, f"Ссылка на твою страницу: {link}")
+    if str == 'inwork':
+        await bot.send_message(callback.from_user.id, f"Выберете:", reply_markup=keyboard_inwork)
+
+async def inwork_function(callback: types.CallbackQuery):
+    await bot.answer_callback_query(callback.id)
+    str = callback.data.split("_")[1]
+    if str == "in":
+        dates = datetime.datetime.today()
+        date = datetime.datetime.now()
+        hour = date.hour
+        minute = date.minute
+        sql_insert = """INSERT INTO inwork (Datatime,Callback, Link, Hour, Minute) VALUES (?,?,?,?,?)"""
+        data_insert = (dates, callback.from_user.id, callback.from_user.username, hour, minute)
+        cur.execute(sql_insert, data_insert)
+        base.commit()
+        await bot.send_message(callback.from_user.id, f"Вы на работе", reply_markup=keyboard_start)
+    if str == "out":
+        dates = datetime.datetime.today()
+        date = datetime.datetime.now()
+        hour = date.hour
+        minute = date.minute
+        sql_insert = """INSERT INTO outwork (Datatime,Callback, Link, Hour, Minute) VALUES (?,?,?,?,?)"""
+        data_insert = (dates, callback.from_user.id, callback.from_user.username, hour, minute)
+        cur.execute(sql_insert, data_insert)
+        base.commit()
+        await bot.send_message(callback.from_user.id, f"До Скорых встреч", reply_markup=keyboard_start)
+
 
 
 if __name__ == '__main__':
     dp.register_message_handler(start, commands=['start'])
     dp.register_callback_query_handler(start_keyboard, Text(startswith ='start_'))
+    dp.register_callback_query_handler(inwork_function, Text(startswith='inwork_'))
     # Запуск бота
     executor.start_polling(dp,skip_updates=True, on_startup=on_startup)
